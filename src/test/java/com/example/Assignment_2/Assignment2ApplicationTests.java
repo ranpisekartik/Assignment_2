63fb8046b778edb23acb1420e5ec5ee8e7a6de4c
unknown <bbaddam@albany.edu>
2018-10-14 23:37:15 -0400

    Assignment 2

---
+++ src/test/java/com/example/Assignment_2/Assignment2ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.Assignment_2;
+
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Assignment2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
